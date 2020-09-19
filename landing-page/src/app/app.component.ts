725f4aee06d8bbcf0f4aa73f23de250616e3ac37
Alexis Hernandez <alexis22229@gmail.com>
2020-09-19 10:36:07 -0600

    landing-page: Fix misspellings on the spanish version

--- landing-page/src/app/app.component.ts
+++ landing-page/src/app/app.component.ts
@@ -23,7 +23,6 @@ We’ll send a notification the instant there’s a better price.`,
       findUsIn: `Find us in`,
       getOnChromeLink: 'Get on Chrome',
       getOnFirefoxLink: 'Get on Firefox (recommended)',
-      getOnMobileText: 'It works on your phone, just install the firefox plugin, the latest features are on Firefox too.',
       blueInstructionsStep1: '1. Find which product you are interested in buying sites',
       youWillNeedTheAppInstalled: `You'll need Discounts Hunter installed`,
       blueInstructionsStep2: `2. Discounts Hunter will ask you if you want <br>to add the product, so you click in ok`,
@@ -36,20 +35,19 @@ We’ll send a notification the instant there’s a better price.`,
     translate.setTranslation('es', {
       appName: 'Caza Descuentos',
       pricesChangePeopleOverpayText: `Los precios cambian.<br>
-Las personas pagan de mas.<br>
-Con Caza Descuentos, tu pagaras menos.`,
+Las personas pagan de más.<br>
+Con Caza Descuentos, tú pagarás menos.`,
       useOurAppOnTheAvailableStores: `Usa Caza Descuentos para monitorear productos de tus tiendas favoritas.
-Te notificaremos en el instante que los productos de tu interes bajen de precio.`,
+Te notificaremos en el instante que los productos de tu interés bajen de precio.`,
       findUsIn: `Encuéntranos en`,
       getOnChromeLink: 'Usar en Chrome',
-      getOnFirefoxLink: 'Usar en Firefox (recomendado)',
-      getOnMobileText: 'Funciona en tu celular si usas Firefox, las mejores funciones tambien.',
+      getOnFirefoxLink: 'Usar en Firefox',
       blueInstructionsStep1: '1. Busca el producto que deseas comprar en las tiendas',
       youWillNeedTheAppInstalled: `Necesitas tener instalada la app de Caza Descuentos`,
       blueInstructionsStep2: `2. Caza Descuentos te preguntara<br>si deseas monitorear el producto`,
       blueInstructionsStep3: `3. Relajate y te notificaremos<br>cuando el producto baje de precio.`,
       availableOn: 'Disponible para',
-      moreStoresSoon: 'Mas tiendas pronto!',
+      moreStoresSoon: 'Más tiendas pronto!',
       getPricedropsWithTheApp: 'Paga menos con Caza Descuentos'
     });
   }
