b526f74477d1c522dfe916e694e91a0f195df0eb
KappaStromblessed <seb02tian20@gmail.com>
2022-08-11 19:53:13 -0600

    lib+pwa: implement NintendoEshop shop

--- lib/common/shared/src/main/scala/net/wiringbits/cazadescuentos/common/models/OnlineStore.scala
+++ lib/common/shared/src/main/scala/net/wiringbits/cazadescuentos/common/models/OnlineStore.scala
@@ -20,6 +20,24 @@ sealed abstract class OnlineStore(
 }
 
 object OnlineStore {
+  final case object NintendoEshopUs
+    extends OnlineStore(
+      "nintendoEshopUs",
+      "Nintendo Eshop",
+      "https://www.nintendo.com/store/products/",
+      "nintendoEshop.png",
+      raw"[\w\W\-]+".r // should match "super-mario-maker-2-switch"
+  )
+
+  final case object NintendoEshopMx
+    extends OnlineStore(
+      "nintendoEshopMx",
+      "Nintendo Eshop",
+      "https://www.nintendo.com/es-mx/store/products/",
+      "nintendoEshop.png",
+      raw"[\w\W\-]+".r // should match "super-mario-maker-2-switch"
+    )
+
   final case object Liverpool
       extends OnlineStore(
         "liverpool",
@@ -244,6 +262,8 @@ object OnlineStore {
       )
 
   val available: List[OnlineStore] = List(
+    NintendoEshopUs,
+    NintendoEshopMx,
     Liverpool,
     HandM,
     HandMMobile,
